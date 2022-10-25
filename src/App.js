194d0fd4be0d4c8dc11d147be7b48abfadf847cb
VictorKalantay <v.kalantay@ukr.net>
2022-10-25 03:59:41 +0300

    Init: Initial project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from "./components/header/Header";
+import {Route, BrowserRouter, Routes} from "react-router-dom";
+import About from "./components/main/About";
+import Skills from "./components/main/Skills";
+import Portfolio from "./components/main/Portfolio";
+import Education from "./components/main/Education";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   return (
+        <BrowserRouter>
+            <div className="App">
+                <Header/>
+                <main>
+                    <Routes>
+                        <Route path=" " element={<About/>}/>
+                        <Route path="/skills" element={<Skills/>}/>
+                        <Route path="/portfolio" element={<Portfolio/>}/>
+                        <Route path="/education" element={<Education/>}/>
+                    </Routes>
+                </main>
+            </div>
+
+
+
+        </BrowserRouter>
+    );
 }
 
 export default App;
