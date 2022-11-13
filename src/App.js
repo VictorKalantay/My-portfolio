bd4984d6f2e7ecc2ed07fbf659fc748050976267
VictorKalantay <v.kalantay@ukr.net>
2022-11-13 19:31:52 +0200

    feat: fix routers

--- src/App.js
+++ src/App.js
@@ -9,31 +9,30 @@ import useThemeChange from "./hooks/useThemeChange";
 import Contacts from "./components/main/Contacts";
 
 
-
-
 function App() {
-    const { handleThemeChangeFromStorage, handleColorChangeFromStorage} = useThemeChange()
+    const {handleThemeChangeFromStorage, handleColorChangeFromStorage} = useThemeChange()
 
     handleColorChangeFromStorage()
     handleThemeChangeFromStorage()
 
 
-       return (
+    return (
 
         <BrowserRouter>
             <div className="App">
                 <Header/>
                 <main>
                     <Routes>
-                        <Route exact path="/My-portfolio/" element={<About/>}/>
-                        <Route exact path="/My-portfolio/skills" element={<Skills/>}/>
-                        <Route exact path="/My-portfolio/portfolio" element={<Portfolio />} />
-                        <Route exact path="/My-portfolio/contacts" element={<Contacts />}/>
+                        <Route path='/My-portfolio/' >
+                        <Route index path="" element={<About/>}/>
+                        <Route path="skills" element={<Skills/>}/>
+                        <Route path="portfolio" element={<Portfolio/>}/>
+                        <Route path="contacts" element={<Contacts/>}/>
+                        </Route>
                     </Routes>
                 </main>
-                <Footer />
+                <Footer/>
             </div>
-
 
 
         </BrowserRouter>
---
+++ src/components/main/MainContent.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { useParams, Link} from 'react-router-dom'
+const MainContent = () => {
+    const urlId = useParams()
+    return (
+        <>
+            
+        </>
+    );
+};
+
+export default MainContent;
