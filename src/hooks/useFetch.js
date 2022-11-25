b26eb32a8b4b46c9c5c9f9c7979f1c6ab0cfd35a
VictorKalantay <v.kalantay@ukr.net>
2022-11-25 12:33:40 +0200

    fix: fix bug with fetch

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -18,6 +18,7 @@ export default function useFetch(baseUrl) {
                 .catch(error => {
                     setLoading(false);
                     reject(error);
+
                 });
         });
     }
--- src/hooks/useIP.js
+++ src/hooks/useIP.js
@@ -5,14 +5,13 @@ import useFetch from "./useFetch";
 const useIP = () => {
     const [userIP, setUserIP] = useState({})
     const {get} = useFetch('https://geolocation-db.com/json/50ad4a90-fd5e-11ec-b463-1717be8c9ff1')
-
-
         useEffect(()=>{
             get('')
                 .then(data => setUserIP(data))
                 .catch(error => {
+                    console.log(error)
                     setUserIP({"country_code":"UA","country_name":"Ukraine","city":"Error get request to IP","postal":"00000","latitude":49.823,"longitude":36.0507,"IPv4":"1.1.1.1","state":"Error get request to IP"})
-                    console.log(error)})
+                  })
         },[])
 return userIP
 
